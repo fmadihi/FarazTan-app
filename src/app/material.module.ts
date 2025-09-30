771e89e9eb332cf0d2c2febf2fae1a4fdab23ab6
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-09-30 14:19:06 +0330

    Add files via upload

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  imports: [MatSlideToggleModule],
-  exports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule,MatButtonModule,MatIconModule],
+  exports: [MatSlideToggleModule,MatButtonModule,MatIconModule],
 })
 export class MaterialModule {}
